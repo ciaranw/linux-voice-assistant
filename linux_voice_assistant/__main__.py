9478c18eacbd6f9b54372a2976426dc32daca280
Ciaran Wood <cyrzinsomnia@gmail.com>
2025-11-09 16:55:02 +0100

    move writing samples into after satellite is woken up

--- linux_voice_assistant/__main__.py
+++ linux_voice_assistant/__main__.py
@@ -333,14 +333,15 @@ def process_audio(state: ServerState):
                         if any(p > 0.5 for oi in oww_onnx_inputs for p in wake_word.process_streaming(oi)): activated = True
                     if activated:
                         now = time.monotonic()
-                        if oww_onnx_features is not None and sample_writer is not None:
-                            sample_writer.write_sample_in_thread(oww_onnx_features.get_audio_buffer())
-
-                        if oww_tflite_features is not None and sample_writer is not None:
-                            sample_writer.write_sample_in_thread(oww_tflite_features.get_audio_buffer())
 
                         if (last_active is None) or ((now - last_active) > state.refractory_seconds):
                             state.satellite.wakeup(wake_word)
+                            if oww_onnx_features is not None and sample_writer is not None:
+                                sample_writer.write_sample_in_thread(oww_onnx_features.get_audio_buffer())
+
+                            if oww_tflite_features is not None and sample_writer is not None:
+                                sample_writer.write_sample_in_thread(oww_tflite_features.get_audio_buffer())
+                                
                             last_active = now
                 if any(state.stop_word.process_streaming(mi) for mi in micro_inputs) and state.stop_word.is_active:
                     state.satellite.stop()
